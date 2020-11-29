890e2daf4aff9c7c777458ea272068272db914b8
Elai Zuberman <elai950@gmail.com>
2020-11-29 17:23:49 +0200

    Initial Commit

---
+++ Shared/Test8200acApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test8200acApp.swift
+//  Shared
+//
+//  Created by אילי זוברמן on 29/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Test8200acApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
